2c4677d927b1cf731868ae98615cd34490a3e04a
Valentin Pichard <valentin.pichard@epitech.eu>
2016-04-25 01:42:19 +0200

    On améliore notre guitte.exe

--- guitte.py
+++ guitte.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-def main_loop():
-    print("Bonjour le monde !")
+import os
+import sys
+
+TRANSLATIONS = {
+    'poule': 'pull',
+}
+
+def main_func():
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        return os.system('git')
+    command = arguments[0]
+    if command not in TRANSLATIONS:
+        return os.system(" ".join(['git'] + arguments))
+    translation = TRANSLATIONS[command]
+    return os.system(" ".join(['git', translation] + arguments[1:]))
 
 if __name__ == '__main__':
-    main_loop()
+    sys.exit(main_func())
